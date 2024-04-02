14c72c6e091bbeac701afafdc54ba70062be2ae9
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-04-02 04:19:25 +0300

    balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,30 @@
-# Install nginx package
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Define the custom HTTP header
-nginx::resource::vhost { 'default':
-  ensure   => present,
-  www_root => '/var/www/html',
-  proxy    => 'http://localhost:8080',
-  custom_fragment => "add_header X-Served-By $::hostname;",
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
