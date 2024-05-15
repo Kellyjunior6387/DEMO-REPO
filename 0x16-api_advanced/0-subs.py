e6fbc26ab8068d2feae8270a21889404a037d99c
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-05-15 03:30:21 +0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
